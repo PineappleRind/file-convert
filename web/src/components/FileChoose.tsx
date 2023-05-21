9ae71a23980a74f22cfa7a300eaeff58c0fc2b08
pr <pineapplerind.info@gmail.com>
2023-05-20 21:57:31 -0400

    feat: support drag&drop

--- web/src/components/FileChoose.tsx
+++ web/src/components/FileChoose.tsx
@@ -8,9 +8,9 @@ interface FileChooseProps {
 
 export default ({ onFileChoose: signal }: FileChooseProps) => {
 	const [file, setFile] = createSignal<File | null>(null);
-	let fileInput;
+	let fileInput, fileDropArea: HTMLDivElement;
 
-	function updateFile(files: FileList | null) {
+	function updateFile(files: File[] | null) {
 		const file = files && files[0];
 		if (file && !validateFile(file))
 			return alert("This file type is not accepted");
@@ -26,12 +26,54 @@ export default ({ onFileChoose: signal }: FileChooseProps) => {
 		return !!exts[type];
 	}
 
+	function handleDrop(e: DragEvent) {
+		e.preventDefault();
+		if (e.dataTransfer?.items) {
+			// Use DataTransferItemList interface to access the file(s)
+			[...e.dataTransfer.items].forEach((item, i) => {
+				// If dropped items aren't files, reject them
+				if (item.kind !== "file") return;
+				const file = item.getAsFile();
+				if (!file) return;
+				updateFile([file]);
+			});
+		} else {
+			// Use DataTransfer interface to access the file(s)
+			[...e.dataTransfer!.files].forEach((file, i) => {
+				updateFile([file]);
+			});
+		}
+	}
+	function handleDragOver(e: DragEvent) {
+		e.preventDefault();
+	}
+
+	function handleDragEnter(e: DragEvent) {
+		fileDropArea.classList.add(styles.dragActive);
+	}
+
+	function handleDragLeave(e: DragEvent) {
+		fileDropArea.classList.remove(styles.dragActive);
+	}
 	return (
-		<div class={styles.fileChoose}>
+		<div
+			class={styles.fileChoose}
+			ondragover={handleDragOver}
+			ondragenter={handleDragEnter}
+			ondragstart={handleDragEnter}
+			ondragleave={handleDragLeave}
+			ondragend={handleDragLeave}
+			ondrop={handleDrop}
+			ref={fileDropArea}
+		>
 			<Show
 				when={file()}
 				fallback={
-					<label class={styles.fileChooseButton} for="fileUpload">
+					<label
+						ondragenter={(e) => e.stopPropagation()}
+						class={styles.fileChooseButton}
+						for="fileUpload"
+					>
 						Choose a file...
 					</label>
 				}
@@ -48,7 +90,9 @@ export default ({ onFileChoose: signal }: FileChooseProps) => {
 				type="file"
 				id="fileUpload"
 				onInput={(e: InputEvent) =>
-					updateFile((e.target as HTMLInputElement).files)
+					updateFile(
+						Array.from((e.target as HTMLInputElement).files as FileList),
+					)
 				}
 				class={styles.nativeFileInput}
 				ref={fileInput}
